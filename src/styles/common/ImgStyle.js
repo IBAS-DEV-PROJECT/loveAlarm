cc5b01cb4de6fe86b3b591f812e7bf26c900ce57
haewonwon <khcw1029@daum.net>
2025-03-07 12:46:42 +0900

    Chore: frontend 폴더 생성

--- src/styles/common/ImgStyle.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Img = styled.img`
-  width: ${({ width }) => width || 'fit-content'};
-  height: ${({ height }) => height || 'fit-content'};
-
-  border: ${({ border }) => border || 'none'};
-`;
